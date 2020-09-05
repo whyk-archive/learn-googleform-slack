1af2839189fc01cc97ce085ac69744f867d8e622
WhyK <windchimeyk@gmail.com>
2020-09-05 11:48:02 +0900

    update: 稼働するコードを追加

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Learn GoogleForm & Slack
 Googleフォームに投稿されたらSlackに通知するGASプロジェクト
+ほぼ参考資料の写経です
+
+あくまで旧Incoming Webhookを使った手法なので、上手く動いたら現Incoming Webhookで書き直す予定
+
+## 参考資料
+- [Googleフォームの内容をSlack通知させて効率化した話](https://qiita.com/poster-keisuke/items/481ed083e1ab0ecb648e)
--- app/Code.ts
+++ app/Code.ts
@@ -1,5 +1,42 @@
-const word: string = 'World'
-Logger.log(`Hello, ${word}!!`)
+import { WebAPICallOptions } from '@slack/web-api';
 
-const exponentiation = (n: number): number => n * n
-Logger.log(exponentiation(5))
+const getEnvProperty = (property: string) => PropertiesService.getScriptProperties().getProperty(property)
+
+const sendToSlack = (text: string, channel: string) => {
+  const url = getEnvProperty('SLACK_CHANNEL_URL')
+  const data: WebAPICallOptions = {
+    channel,
+    username: 'テスト通知',
+    attachments: [
+      {
+        color: '#000',
+        title: 'テスト投稿がされました',
+        text
+      }
+    ],
+    icon_url: 'http://2.bp.blogspot.com/-Rd_w7b2kq70/VnE4FZen5oI/AAAAAAAA18w/mMasxPYIuCM/s800/pose_kyosyu_boy.png'
+  }
+  const payload = JSON.stringify(data)
+  const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    method: 'post',
+    'contentType': 'application/json',
+    payload
+  }
+  const response = UrlFetchApp.fetch(url, options)
+}
+
+const onFormSubmit = (e: GoogleAppsScript.Events.FormsOnFormSubmit) => {
+  const itemResponse = e.response.getItemResponses()
+  let category: string | string[] | string[][],
+      content: string | string[] | string[][]
+  itemResponse.forEach(res => {
+    const title = res.getItem().getTitle()
+    const response = res.getResponse()
+
+    if (title === 'カテゴリ') category = response
+    else if (title === '内容') content = response
+
+  })
+  const body = `質問カテゴリ:【${category}】\n\n内容:\n${content}`
+  sendToSlack(body, getEnvProperty('CHANNEL_NAME'))
+}
